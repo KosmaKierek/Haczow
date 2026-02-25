96d7d8761e0d645f2fc7913d7fad31815fc936c6
Kosma Kierek <kosma.kierek@host.com>
2026-02-25 01:34:43 +0100

    fresh

---
+++ public/templates/ubieran.js
@@ -0,0 +1,101 @@
+function nexthat() {
+  var x = document.getElementById("hat");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function nextvest() {
+  var x = document.getElementById("vest");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function nextbelt() {
+  var x = document.getElementById("belt");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function nextpants() {
+  var x = document.getElementById("pants");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function nextsuit() {
+  var x = document.getElementById("suit");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function nextboots() {
+  var x = document.getElementById("boots");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+
+
+function nextchusta() {
+  var x = document.getElementById("chusta");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function nextkamizelka() {
+  var x = document.getElementById("kamizelka");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function nextfartuch() {
+  var x = document.getElementById("fartuch");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function nextbuty() {
+  var x = document.getElementById("buty");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
+function nextsuknia() {
+  var x = document.getElementById("suknia");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
